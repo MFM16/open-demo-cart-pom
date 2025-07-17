5d89e3e842f9da8721aa168b4f92b647fc23fcff
FarhanMaulidian <farhanmaulidian16@gmail.com>
2025-07-17 12:40:31 +0700

    Add xml file and config file

--- src/test/java/testCases/BaseTest.java
+++ src/test/java/testCases/BaseTest.java
@@ -5,24 +5,55 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseTest {
     public WebDriver driver;
     public Logger logger;
+    public Properties property;
 
     @BeforeClass
-    public void setup(){
+    @Parameters({"os", "browser"})
+    public void setup(String os, String browser) throws IOException {
+        FileReader file = new FileReader("./src/test/resources/config.properties");
+        property = new Properties();
+        property.load(file);
+
         logger = LogManager.getLogger(this.getClass());
 
-        driver = new ChromeDriver();
+        switch (browser.toLowerCase()){
+            case "chrome" :
+                driver = new ChromeDriver();
+                logger.info("Testing in Chrome Browser");
+                break;
+            case "firefox" :
+                driver = new FirefoxDriver();
+                logger.info("Testing in Firefox Browser");
+                break;
+            case "edge" :
+                driver = new EdgeDriver();
+                logger.info("Testing in Edge Browser");
+                break;
+            default:
+                System.out.println("Invalid browser");
+                return;
+        }
+
+
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        driver.get("http://localhost:8080/opencart/");
+        driver.get(property.getProperty("appUrl"));
     }
 
     @AfterClass

ca6056b26b2c2de6438eb247ccd51b2376305dae
FarhanMaulidian <farhanmaulidian16@gmail.com>
2025-07-17 11:39:28 +0700

    Add logger to test case

--- src/test/java/testCases/RegistrationTest/TC001_AccountRegistration.java
+++ src/test/java/testCases/RegistrationTest/TC001_AccountRegistration.java
@@ -9,11 +9,16 @@ import testCases.BaseTest;
 public class TC001_AccountRegistration extends BaseTest {
     @Test
     public void verifyAccountRegistration(){
+        logger.info("---------- Test TC001_AccountRegistration ----------");
+
         HomePage home = new HomePage(driver);
         home.clickLinkMyAccount();
+        logger.info("Click link my account !");
         home.clickLinkRegister();
+        logger.info("Click link register !");
 
         RegistrationPage regis = new RegistrationPage(driver);
+        logger.info("Providing information details !");
         regis.setTxtFirstName(randomAlphabet());
         regis.setTxtLastName(randomAlphabet());
         regis.setTxtEmail(randomAlphabet()+"@gmail.com");
@@ -22,6 +27,14 @@ public class TC001_AccountRegistration extends BaseTest {
         regis.clickBtnContinue();
 
         String confirmationMsg = regis.getConfirmationMessage();
-        Assert.assertEquals(confirmationMsg, "Your Account Has Been Created!");
+        if (confirmationMsg.equals("Your Account Has Been Created!")){
+            Assert.assertTrue(true);
+            logger.info("Test Case Success !");
+        } else {
+            Assert.fail();
+            logger.info("Test Case Failed !");
+        }
+
+        logger.info("---------- Finished TC001_AccountRegistration ----------");
     }
 }
